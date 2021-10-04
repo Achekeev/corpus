a1b7be22b57bf14e795be246ba088900cf84bac1
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-04 22:54:06 +0600

    Create About page

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import Header from "components/Header";
+import Footer from "components/Footer";
+
+const About = () => {
+    return (
+        <>
+            <div className="container">
+                <Header/>
+            </div>
+
+            <div className="main-image_about">
+                <div className="main-image-filter">
+                    <div className="container">
+                        <h1>О нас</h1>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo atque
+                            a similique! Ducimus soluta adipisci mollitia. Dolorem inventore
+                            officia aut obcaecati exercitationem sit, doloremque nulla quas
+                            laudantium dolore ab adipisci?
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="company">
+                    <div className="com-item">
+                        <h1>Nulla sed sit.</h1>
+                        <p className="com-desc">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Velit sapien id lacinia non felis scelerisque dictum nec dolor.
+                            Iaculis ipsum pellentesque quam accumsan sem sapien urna sed scelerisque.
+                            Consectetur nibh neque ullamcorper sollicitudin facilisi ultrices amet turpis in.
+                            Tincidunt quam proin dictum et parturient sagittis,
+                            ultrices pellentesque scelerisque. Tellus in id vel,
+                            eget sem amet duis consequat fermentum. Auctor diam fermentum sed ultricies.
+                            Ut ut nisi turpis augue mi. Ac fermentum.
+                        </p>
+                    </div>
+                    <div className="com-item">
+                        <div className="com-img">
+                            <div className="img-item">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </div>
+                        </div>
+                        <div className="com-img">
+                            <div className="img-item">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+                            </div>
+                            <div className="img-item">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="center">
+
+                </div>
+                <p className="com-end">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue tristique quis id magna arcu,
+                    porttitor ornare. Vestibulum pulvinar interdum scelerisque sagittis. Auctor suspendisse libero
+                    tincidunt scelerisque. Et sit amet, purus tincidunt amet, pellentesque imperdiet et magnis. Orci
+                    nunc hendrerit varius nisi aliquet libero turpis ornare arcu. Quam integer lectus in dictum et,
+                    ultrices mollis est tortor. Nibh porttitor congue id arcu et nibh amet massa gravida. Integer odio
+                    malesuada quis mi, vestibulum, in. Netus sagittis massa proin at quis sed aliquam. Aliquam fermentum
+                    et, vitae facilisis neque imperdiet tincidunt odio. Viverra amet habitant sed nulla magna egestas
+                    vel non sociis. Donec ullamcorper in sed posuere id commodo odio duis. Nunc cursus eu morbi nibh
+                    egestas iaculis proin. Ipsum integer sit diam aenean.
+                    Etiam sit phasellus posuere semper et ipsum hac ut aliquet. Amet posuere id nisi, dis aliquam non,
+                    tempus. Ac mauris sit volutpat turpis quis. Pharetra sit morbi semper convallis egestas purus
+                    iaculis auctor in. Et sed suspendisse a facilisis diam. Augue ornare semper cursus nisl enim
+                    condimentum risus. Nulla semper lacus magna justo, ullamcorper. Dui nulla euismod consectetur
+                    vehicula rhoncus. Tincidunt aliquet porttitor facilisis aliquam bibendum est leo. Cursus sapien mus
+                    viverra eu non aliquam vitae facilisi vulputate. Dui condimentum ut arcu cursus in. Turpis amet,
+                    egestas nunc risus, egestas ultrices nisl. Vestibulum venenatis, proin donec risus, congue.
+                    Dis adipiscing semper lacus scelerisque leo vitae. Mollis vitae egestas nec tempor vitae tellus.
+                    Aliquet facilisi tristique eget feugiat est leo ultricies massa a. In malesuada duis amet, et ac vel
+                    in elementum. Ipsum elit, euismod sit posuere. Egestas eget sit eget augue semper.
+                </p>
+            </div>
+
+            <Footer/>
+        </>
+    );
+};
+
+export default About;
