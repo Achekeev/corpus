3364d29ad56ea87d5a871152628de80b34cbe553
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-10 16:13:35 +0600

    Clicking on cart image now redirects to /cart page

--- src/components/Header/Cart.tsx
+++ src/components/Header/Cart.tsx
@@ -1,19 +1,16 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import busket from "assets/img/busket.png";
-import {useAppActions, useAppDispatch, useAppSelector} from "hooks";
+import {useAppSelector} from "hooks";
+import {useHistory} from "react-router-dom";
 
 const Cart = () => {
-    const dispatch = useAppDispatch()
+    // const dispatch = useAppDispatch()
+    const history = useHistory()
     const {orders} = useAppSelector(state => state.cart)
-    const {addOrder} = useAppActions(actions => actions.cart)
-
-    useEffect(() => {
-        dispatch(addOrder(20))
-        dispatch(addOrder(20))
-    }, [addOrder, dispatch])
+    // const {addOrder} = useAppActions(actions => actions.cart)
 
     return (
-        <div className="cart" onClick={() => dispatch(addOrder(25))}>
+        <div className="cart" onClick={() => history.push("/cart")} style={{cursor: "pointer"}}>
             <img src={busket} alt=""/>
             <div className="counter">{orders.length}</div>
         </div>
