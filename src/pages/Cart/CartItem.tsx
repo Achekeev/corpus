757b3a6edfee8e38e594261867db5afecd778383
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-10 18:50:18 +0600

    Use cart item amount increase/decrease and remove functionality

--- src/pages/Cart/CartItem.tsx
+++ src/pages/Cart/CartItem.tsx
@@ -1,44 +1,55 @@
 import React from 'react';
 import cartImage2 from "assets/img/cab-img-2.svg";
+import {useAppActions, useAppDispatch, useAppSelector} from "hooks";
 
-const CartItem = () => {
+interface CartItemProps {
+    productID: number
+}
+
+const CartItem = ({productID}: CartItemProps) => {
+    const dispatch = useAppDispatch()
+
+    const order = useAppSelector(state => state.cart.orders.find(el => el.productID === productID))
+    const product = useAppSelector(state => state.products.products.filter(el => el.id === productID)[0])
+    const {increaseAmount, decreaseAmount, removeOrder} = useAppActions(actions => actions.cart)
+
     return (
         <div className="cab-item">
             <div>
-                <img src={cartImage2} alt=""/>
+                <img src={product.image} alt="" width="300px"/>
             </div>
             <div className="cab-info">
                 <div className="info">
                     <div>
                         <p className="pro-name">
-                            Диван маленький
+                            {product.description}
                         </p>
                         <p className="prod-num">
-                            #564644515648445
+                            {product.vendor_code}
                         </p>
                     </div>
                     <div>
                         <p className="price">
-                            230000
+                            {product.price}
                         </p>
                     </div>
                 </div>
                 <div className="info">
                     <div className="prod-count">
-                        <button className="counter-btn">
+                        <button className="counter-btn" onClick={() => dispatch(decreaseAmount(productID))}>
                             -
                         </button>
 
                         <em className="value">
-                            0
+                            {order?.amount}
                         </em>
 
-                        <button className="counter-btn">
+                        <button className="counter-btn" onClick={() => dispatch(increaseAmount(productID))}>
                             +
                         </button>
                     </div>
                     <div className="cab-values">
-                        <button className="delete-btn">
+                        <button className="delete-btn" onClick={() => dispatch(removeOrder(productID))}>
                             Удалить
                         </button>
                     </div>
