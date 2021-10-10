3d2b7be20ce6c1f89f7fc30e848d5c6f7674e834
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-10 18:54:49 +0600

    Show in a list of orders only products that still exist

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Header from "components/Header";
 import Features from "components/Features";
 import CartItem from "pages/Cart/CartItem";
+import {useAppSelector} from "hooks";
 
 const Cart = () => {
+    const {products} = useAppSelector(state => state.products)
+    const orders = useAppSelector(state => state.cart.orders.filter(order => products.find(p => p.id === order.productID)))
+
     return (
         <div className="container">
             <Header/>
@@ -14,7 +18,7 @@ const Cart = () => {
                 </h1>
                 <hr className="cab-line"/>
 
-                {[...Array(10)].map((x, i) => <CartItem key={i}/>)}
+                {orders.map((x, i) => <CartItem productID={x.productID} key={i}/>)}
                 <button className="order">
                     Оформить заказ
                 </button>
