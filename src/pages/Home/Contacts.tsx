cb0a9547c2144e37ba2c0af05a48cd0aab4fbe7a
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-16 15:31:23 +0600

    Put new contacts

--- src/pages/Home/Contacts.tsx
+++ src/pages/Home/Contacts.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import whatsAppImage from "assets/img/wa.svg"
 import telegramImage from "assets/img/telegram.svg"
-import mailImage from "assets/img/mail.svg"
+// import mailImage from "assets/img/mail.svg"
 
 const Contacts = () => {
+    const email = "corpus@gmail.com"
+    const phoneNumber = "+996 312 909 010"
+    const whatsAppNumber = "996556052952"
     return (
         <section className="contacts" id="contacts">
-            <a href="mailto:corpus@gmail.com">corpus@gmail.com</a>
-            <a href="tel:+996700500500">+996 700 500 500</a>
+            <a href={`mailto:${email}`}>{email}</a>
             <a href="/">
-                <img src={whatsAppImage} alt="" className="wa" />
+                Режим работы: 09:00-18:00, СБ/ВС-выходные
+            </a>
+            <a href={`tel:${phoneNumber}`}>{phoneNumber}</a>
+            <a href={`https://wa.me/${whatsAppNumber}`}>
+                <img src={whatsAppImage} alt="" className="wa"/>
             </a>
             <a href="/">
-                <img src={telegramImage} alt="" />
-            </a>
-            <a href="/">
-                <img src={mailImage} alt="" />
+                <img src={telegramImage} alt=""/>
             </a>
         </section>
     );
