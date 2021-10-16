1774138ef25e26a6b8920f6db953feb203eeee99
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-16 16:29:02 +0600

    API HOST moved into .env file

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios"
 
+const baseURL = process.env.REACT_APP_API_HOST
+
 export const productsAPI = axios.create({
-    baseURL: "http://localhost:8000/products/"
+    baseURL: `${baseURL}/products/`
 })
