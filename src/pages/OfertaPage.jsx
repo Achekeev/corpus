9a5d65b609367f383d5c0b633ee918de8dc09c84
Elberat <elberatusmanov.web@gmail.com>
2022-01-15 03:49:20 +0600

    oferta page

--- src/pages/OfertaPage.jsx
+++ src/pages/OfertaPage.jsx
@@ -1,30 +1,59 @@
 import React from "react";
-import styles from "./styles/OfertaPage.module.css";
+import styles from "./styles/AboutPage.module.css";
+import stylesBr from "./styles/breadcrumbs.module.css";
+import { Link as LinkRouter } from "react-router-dom";
+
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import Image from "../assets/img/Depositphotos_32696225_l-2015_result.jpg";
 
 const OfertaPage = () => {
     return (
-        <div className={styles.container_oferta}>
-            <h1>oferta</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
-                necessitatibus accusantium dolor corrupti expedita rerum dolores
-                exercitationem obcaecati, optio harum nihil quaerat molestias
-                earum molestiae consequatur adipisci, labore magnam soluta
-                consequuntur eum cum quasi voluptates illum? Ullam perferendis
-                dicta rerum. Amet molestias quos et odit praesentium earum, a
-                incidunt dolorum?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                blanditiis aliquid est voluptatum accusamus voluptas non
-                cupiditate ipsum? Ullam, iste.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                blanditiis aliquid est voluptatum accusamus voluptas non
-                cupiditate ipsum? Ullam, iste.
-            </p>
-        </div>
+        <>
+            <div className={stylesBr.breadcrumbs_container}>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link underline="hover" color="inherit">
+                        <LinkRouter to="/">Главная</LinkRouter>
+                    </Link>
+                    <Link underline="hover" color="inherit">
+                        <LinkRouter to="/cart">Публичная оферта</LinkRouter>
+                    </Link>
+                </Breadcrumbs>
+            </div>
+
+            <h2 className={styles.h2}>Публичная оферта</h2>
+            <div className={styles.about_container}>
+                <img src={Image} alt="about page" />
+                <div className={styles.text}>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Fugiat, doloribus!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Ipsam asperiores eveniet nesciunt accusantium
+                        adipisci provident voluptates quos possimus minima
+                        aspernatur architecto libero fuga eos a expedita,
+                        pariatur vero repellendus unde!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Officia nam quia ad mollitia! Labore consequuntur
+                        a doloribus mollitia tempore vero commodi nemo libero.
+                        Cum, praesentium consequatur dolore quis quas fugiat
+                        quod minima ratione voluptate cumque inventore voluptas
+                        magni beatae veniam?
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Magnam voluptatibus dolorum possimus impedit ab
+                        perferendis ipsam, expedita quia consectetur repellat
+                        quae soluta minus quas assumenda, et vel molestias.
+                        Adipisci, omnis?
+                    </p>
+                </div>
+            </div>
+        </>
     );
 };
 
